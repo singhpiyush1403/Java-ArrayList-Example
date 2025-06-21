d8702a70688935df7493f3c2d4d484227d8c33fc
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-06-21 18:55:27 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,136 @@
+
+// import java.util.ArrayList;
+// public class Mian{
+//     public static void main(String args[]){
+//         ArrayList<Integer> list = new ArrayList<Integer>();
+//         list.add(10);
+//         list.add(20);
+//         list.add(30);
+//         list.add(40);
+//         System.out.println(list);
+//     }
+// }
+
+
+
+// import java.util.ArrayList;
+// public class Mian{
+//     public static void main(String args[]){
+//         ArrayList<Integer> list = new ArrayList<Integer>();
+//         list.add(10);
+//         list.add(20);
+//         list.add(30);
+//         list.add(40);
+//         System.out.println(list);
+        
+//         // get element
+//         int element = list.get(1);
+//         System.out.println(element);
+//         }
+// }
+
+
+import java.util.ArrayList;
+public class Mian{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+        
+        // add element in between
+        list.add(1,1);
+        System.out.println(list);
+    }
+}
+
+
+import java.util.ArrayList;
+public class Mian{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+        
+        // set element
+        list.set(0,5);
+        System.out.println(list);
+    }
+}
+
+
+
+import java.util.ArrayList;
+public class Mian{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+ 
+        // remove element
+        list.remove(2);
+        System.out.println(list);
+    }
+}
+
+
+import java.util.ArrayList;
+public class Mian{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+        // size
+        int size = list.size();
+        System.out.println(size);
+    }
+}
+
+
+import java.util.ArrayList;
+public class Mian{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+        // Loops
+        for(int i=0; i<list.size(); i++){
+            System.out.print(list.get(i));
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
